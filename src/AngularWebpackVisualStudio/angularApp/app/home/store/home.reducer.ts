a2006eeefe9deecf665a4db527b328a4cb695ca0
damienbod <damien_bod@hotmail.com>
2017-09-05 14:49:40 +0200

    adding a reducer

--- src/AngularWebpackVisualStudio/angularApp/app/home/store/home.reducer.ts
+++ src/AngularWebpackVisualStudio/angularApp/app/home/store/home.reducer.ts
@@ -1,7 +1,24 @@
 import { HomeState } from './home.state';
 import { Thing } from './../../models/thing';
+import { Action } from "@ngrx/store";
+import * as thingsAction from './thing.action';
 
 export const initialState: HomeState = {
     things: [],
     selectedThing: new Thing()
 };
+
+export function thingsReducer(state = initialState, action: thingsAction.Actions): HomeState {
+    switch (action.type) {
+
+        //case thingsAction.AddCompleteAction: {
+        //    return Object.assign({}, state, {
+        //        foodItems: state.things.concat(thingsAction.AddCompleteAction.things),
+        //        selectedItem: new Thing()
+        //    });
+        //}
+        default:
+            return state;
+
+    }
+}
