4ebc9a56b360a8de3297ec5b640aa93d4c78d3b1
damienbod <damien_bod@hotmail.com>
2017-09-05 14:06:38 +0200

    Adding actions for the things

---
+++ src/AngularWebpackVisualStudio/angularApp/app/home/store/thing.action.ts
@@ -0,0 +1,91 @@
+import { Action } from '@ngrx/store';
+import { Thing } from './../../models/thing';
+
+export const ADD = '[Thing] Add';
+export const ADD_COMPLETE = '[Thing] Add Complete';
+
+export const UPDATE = '[Thing] Update';
+export const UPDATE_COMPLETE = '[Thing] Update Complete';
+
+export const DELETE = '[Thing] Delete';
+export const DELETE_COMPLETE = '[Thing] Delete Complete';
+
+export const SELECT = '[Thing] Select';
+export const SELECT_COMPLETE = '[Thing] Select Complete';
+
+export const SELECTALL = '[Thing] Select All';
+export const SELECTALL_COMPLETE = '[Thing] Select All Complete';
+
+export class AddAction implements Action {
+    readonly type = ADD;
+
+    constructor(public payload: string) { }
+}
+
+export class AddCompleteAction implements Action {
+    readonly type = ADD_COMPLETE;
+
+    constructor(public payload: Thing[]) { }
+}
+
+export class UpdateAction implements Action {
+    readonly type = UPDATE;
+
+    constructor(public payload: string) { }
+}
+
+export class UpdateCompleteAction implements Action {
+    readonly type = UPDATE_COMPLETE;
+
+    constructor(public payload: Thing[]) { }
+}
+
+export class DeleteAction implements Action {
+    readonly type = DELETE;
+
+    constructor(public payload: string) { }
+}
+
+export class DeleteCompleteAction implements Action {
+    readonly type = DELETE_COMPLETE;
+
+    constructor(public payload: Thing[]) { }
+}
+
+export class SelectAction implements Action {
+    readonly type = SELECT;
+
+    constructor(public payload: string) { }
+}
+
+export class SelectCompleteAction implements Action {
+    readonly type = SELECT_COMPLETE;
+
+    constructor(public payload: Thing[]) { }
+}
+
+export class SelectAllAction implements Action {
+    readonly type = SELECTALL;
+
+    constructor(public payload: string) { }
+}
+
+export class SelectAllCompleteAction implements Action {
+    readonly type = SELECTALL_COMPLETE;
+
+    constructor(public payload: Thing[]) { }
+}
+
+export type Actions
+    = AddAction
+    | AddCompleteAction
+    | UpdateAction
+    | UpdateCompleteAction
+    | DeleteAction
+    | DeleteCompleteAction
+    | SelectAction
+    | SelectCompleteAction
+    | SelectAllAction
+    | SelectAllCompleteAction;
+
+
