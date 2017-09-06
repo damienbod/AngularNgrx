30e87d1ad029f9660367c433f9e687685f5f7303
damienbod <damien_bod@hotmail.com>
2017-09-06 13:49:05 +0200

    Adding store stuff to the component

--- src/AngularWebpackVisualStudio/angularApp/app/countries/components/country.component.ts
+++ src/AngularWebpackVisualStudio/angularApp/app/countries/components/country.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { CountryState } from '../store/country.state';
+import * as CountryActions from '../store/country.action';
+import { Country } from './../../models/country';
 
 @Component({
     selector: 'app-country-component',
     templateUrl: './country.component.html'
 })
 
-export class CountryComponent {
+export class CountryComponent implements OnInit {
 
-    public message: string;
+    public async: any;
 
-    constructor() {
-        this.message = 'Hello from CountryComponent constructor';
+    countryState$: Observable<CountryState>;
+
+    constructor(private store: Store<any>) {
+        this.countryState$ = this.store.select<CountryState>(state => state.country.countries);
+    }
+
+    ngOnInit() {
+        this.store.dispatch(new CountryActions.SelectAllAction());
     }
 }
