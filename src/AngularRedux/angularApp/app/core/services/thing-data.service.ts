c03540ada84649c18817f23f4e526dae66f3ba50
damienbod <damien_bod@hotmail.com>
2017-12-10 11:01:09 +0100

    removing old rxjs imports

--- src/AngularRedux/angularApp/app/core/services/thing-data.service.ts
+++ src/AngularRedux/angularApp/app/core/services/thing-data.service.ts
@@ -1,6 +1,4 @@
-﻿import 'rxjs/add/operator/map';
-
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+﻿import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
